144dfb59dc806a78b41c49f5514a6140048f5ec6
nairobi77 <sobakko@gmail.com>
2018-05-11 18:46:12 +0300

    fix some compile errors

--- src/app/passport-page-view/passport-page-view.component.ts
+++ src/app/passport-page-view/passport-page-view.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ChangeStateEventEmitterService} from "../change-state-event-emitter.service";
+import {ChangeStateEventEmitterService} from '../change-state-event-emitter.service';
 
 
 @Component({
@@ -9,27 +9,27 @@ import {ChangeStateEventEmitterService} from "../change-state-event-emitter.serv
 })
 export class PassportPageViewComponent implements OnInit {
   private pageByType = {
-    "2-3": "page1",
-    "4-5": "page2",
-    "6-7": "page3",
-    "8-9": "page4",
-    "10-11": "page5",
-    "12-13": "page6",
-    "14-15": "page7",
-    "16-17": "page8",
-    "18-19": "page9"
+    '2-3': 'page1',
+    '4-5': 'page2',
+    '6-7': 'page3',
+    '8-9': 'page4',
+    '10-11': 'page5',
+    '12-13': 'page6',
+    '14-15': 'page7',
+    '16-17': 'page8',
+    '18-19': 'page9'
   };
 
   @Input()
-  type = false;
+  type: string = undefined;
 
   @Input()
   page: any;
 
-  backgroundColor: string = 'white';
+  backgroundColor = 'white';
 
   constructor(private changeStateEventEmitterService: ChangeStateEventEmitterService) {
-    changeStateEventEmitterService.emitter.subscribe((data) => this.stateUpdated(data))
+    changeStateEventEmitterService.emitter.subscribe((data) => this.stateUpdated(data));
   }
 
   stateUpdated(state) {
@@ -39,7 +39,6 @@ export class PassportPageViewComponent implements OnInit {
         if (pageName in state.passport) {
           this.page = state.passport[pageName];
           this.updateBackground();
-
         }
       }
     }
@@ -48,14 +47,14 @@ export class PassportPageViewComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.page);
     if (this.type && !this.page) {
-      this.page = {"href": require("./img/" + this.type + ".png")};
+      this.page = {'href': require('./img/' + this.type + '.png')};
     }
     this.updateBackground();
   }
 
   private updateBackground() {
-    if(this.page) {
-      if('state' in this.page) {
+    if (this.page) {
+      if ('state' in this.page) {
         switch (this.page.state) {
           case 'error':
             this.backgroundColor = 'red';
